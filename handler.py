a871e13dfdf9407b9ac80cfb96d21cc9fa3be65f
KeremP <kerpro26@gmail.com>
2023-01-25 14:19:39 -0500

    some bug fixes after testing in AWS lambda environment. added light documentation. improved logging.

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import uuid
 import json
-import requests
 import tarfile
+import subprocess
 from typing import Optional, Union
 from dotenv import load_dotenv
 import logging
@@ -15,13 +16,12 @@ load_dotenv()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+import urllib3
 import redis
 from redis import UsernamePasswordCredentialProvider
 import numpy as np
 
 import hashlib
-import urllib.request
-from bs4 import BeautifulSoup
 
 import openai
 from langchain import OpenAI
@@ -42,7 +42,14 @@ REDIS_DB   = 0
 
 USE_REDIS = bool(int(os.getenv("USE_REDIS")))
 
+DEBUG = bool(int(os.getenv("DEBUG")))
+
+
 class CustomFAISS(FAISS):
+    """
+    Extension of langchain `FAISS` class to use cached embeddings from a Redis instance
+
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
@@ -69,7 +76,7 @@ class CustomFAISS(FAISS):
                 if redis is not None:
                     redis.set(doc_hash, json.dumps(embeddings))
             except Exception as e:
-                print(f"Unable to cache embeddings in Redis: {e}")
+                logger.warning(f"Unable to cache embeddings in Redis: {e}")
         else:
             embeddings = CustomFAISS.decode_cached_embeddings(precomputed_embeddings)
 
@@ -85,38 +92,64 @@ class CustomFAISS(FAISS):
         return cls(embedding.embed_query, index, docstore, index_to_id)
 
 
-def gunzip(path: str, temp_dir: str):
+def gunzip(path: str, temp_dir: str, sub_dir: str):
+    """
+    Unzip gzipped tarballs
+    """
     file = tarfile.open(path)
-    file.extractall(os.path.join("./temp", temp_dir))
+    file.extractall(os.path.join(temp_dir, sub_dir))
     file.close()
 
 def cleanup(path: str):
     os.remove(path)
 
-def get_gzip_source_files(arxiv_id: str):
+def download_stream(resp, path):
+    CHUNK_SIZE = 1024*8
+    with open(path, 'wb') as out:
+        while True:
+            data = resp.read(CHUNK_SIZE)
+            if not data:
+                break
+            out.write(data)
+    resp.release_conn()
+
+def get_gzip_source_files(arxiv_id: str, parent_path: str):
+    """
+    Download document source files from arxiv, save to tmp directory, unzip, and return name of directory where files are saved
+    """
     base_url = "https://arxiv.org/e-print/"
     target = base_url+arxiv_id
     joined_id = "".join(arxiv_id.split("."))
-    filename = joined_id + ".tar.gz"
-    urllib.request.urlretrieve(target, filename)
-    gunzip(filename, joined_id)
+
+    filename = parent_path + joined_id + ".tar.gz"
+    http = urllib3.PoolManager()
+    resp = http.request('GET', target, preload_content=False)
+    download_stream(resp, filename)
+    gunzip(filename, parent_path, joined_id)
     cleanup(filename)
     return joined_id
 
-def init_redis(host: str, port: int, db: int, username: Optional[str] = None, password: Optional[str] = None) -> Union[redis.Redis, None]:
+def init_redis(host: str, port: int, username: Optional[str] = None, password: Optional[str] = None, ssl: bool = True) -> Union[redis.Redis, None]:
+    """
+    Initialize Redis instance @host:port
+
+    Optionally pass `username` and `password` for auth, and `ssl` if needed
+
+    By default `decode_responses` is set to False
+    """
     if username and password:
         cred_provider = UsernamePasswordCredentialProvider(username, password)
     else:
         cred_provider = None
     
-    client = redis.Redis(host, port, db, credential_provider=cred_provider, decode_responses=False)
+    client = redis.Redis(host, port, credential_provider=cred_provider, decode_responses=False, ssl=ssl)
     try:
         pong = client.ping()
     except:
         pong = None
     
     if pong:
-        print("Redis initialized")
+        logger.info("Redis initialized")
         return client
     else:
         raise Exception("Unable to connect to Redis server")
@@ -129,7 +162,6 @@ def hash_doc(arxiv_id):
 @timer_func
 def build_docstore(context, metadata, doc_hash, redis):
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_KEY)
-    # docsearch = FAISS.from_texts(context, embeddings, metadatas=metadata)
     docsearch = CustomFAISS.from_precomputed(embeddings, context, metadatas=metadata, redis=redis, doc_hash=doc_hash)
     return docsearch
  
@@ -152,9 +184,15 @@ def build_context(parsed_tex: dict):
 
 def lambda_handler(event, context):
     if USE_REDIS:
-        REDIS_CLIENT: redis.Redis = init_redis(REDIS_HOST, REDIS_PORT, REDIS_DB)
+        logger.info("Using REDIS")
+        if DEBUG: ssl = False 
+        else: ssl = True
+        REDIS_CLIENT: redis.Redis = init_redis(REDIS_HOST, REDIS_PORT, REDIS_DB, ssl=ssl)
     else:
         REDIS_CLIENT = None
+
+    if DEBUG: parent_path = "./temp"
+    else: parent_path = "/tmp/"
 
     result = None
     action = event.get('ids')
@@ -163,19 +201,18 @@ def lambda_handler(event, context):
     id_tex_map = {}
     doc_hashes = []
     for id in action:
-       clean_id = get_gzip_source_files(id)
-       files = os.listdir(os.path.join("./temp", clean_id))
-       tex_path = [f for f in files if ".tex" in f][0]
-       tex_path_dir = os.path.join("./temp",clean_id,tex_path)
-       parsed_tex = parse_tex(tex_path_dir)
-       id_tex_map.update({clean_id:parsed_tex})
-       doc_hashes.append(
-        hash_doc(id)
-       )
+        clean_id = get_gzip_source_files(id, parent_path)
+        files = os.listdir(os.path.join(parent_path, clean_id))
+        tex_path = [f for f in files if ".tex" in f][0]
+        tex_path_dir = os.path.join(parent_path,clean_id,tex_path)
+        parsed_tex = parse_tex(tex_path_dir)
+        id_tex_map.update({clean_id:parsed_tex})
+        doc_hashes.append(
+            hash_doc(id)
+        )
 
     context, context_map, metadata = build_context(id_tex_map[list(id_tex_map.keys())[0]])
     
-    # pp.pprint(context_map)
     docstore = build_docstore(context, metadata, doc_hashes[0], REDIS_CLIENT)
     
     chain = VectorDBQAWithSourcesChain.from_llm(OpenAI(temperature=0), vectorstore=docstore)
